272bbb8a65817fdaa1fe4308774be37acb5ed389
Ryan Southgate <ryansouthgate@live.com>
2016-10-14 14:49:04 +0100

    added window.alert

--- WebApp/src/WebApp/app/searchbox.component.ts
+++ WebApp/src/WebApp/app/searchbox.component.ts
@@ -4,7 +4,7 @@
 @Component({
     selector: 'search-box',
     template: `<div>
-                    <input type="text" placeholder="lets get searching..." />
+                    <input #box type="text" (keyup.enter)="displayAlert(box.value)" placeholder="lets get searching..." />
                </div>`
 })
 export class SearchboxComponent {
@@ -12,5 +12,9 @@ export class SearchboxComponent {
     text = "Search Here";
     constructor() {
         this.text;
+    }
+
+    displayAlert(msg: string) {
+        window.alert(msg);
     }
 }
