8a2180421b61501e235b300778df89b66d8a5e07
Ryan Southgate <ryansouthgate@live.com>
2016-10-17 14:52:00 +0100

    tidied gulpfile

--- WebApp/src/WebApp/gulpfile.js
+++ WebApp/src/WebApp/gulpfile.js
@@ -18,39 +18,30 @@ gulp.task('default', function () {
 
 gulp.task('beforeBuild', function () {
     // Copy node_module js files across
-    gulp.src([paths.nodeModules + 'zone.js/dist/zone.js'])
-        .pipe(gulp.dest(webRoot + 'js/'))
+    gulp.src([paths.nodeModules + 'zone.js/dist/zone.js',
+              paths.nodeModules + 'reflect-metadata/Reflect.js',
+              paths.nodeModules + 'core-js/client/shim.min.js',
+              paths.nodeModules + 'systemjs/dist/system.src.js'])
+        .pipe(gulp.dest(webRoot + 'js/'));
 
-    gulp.src([paths.nodeModules + 'reflect-metadata/Reflect.js'])
-        .pipe(gulp.dest(webRoot + 'js/'))
-
-    gulp.src([paths.nodeModules + 'core-js/client/shim.min.js'])
-        .pipe(gulp.dest(webRoot + 'js/'))
-
-    gulp.src([paths.nodeModules + 'systemjs/dist/system.src.js'])
-        .pipe(gulp.dest(webRoot + 'js/'))
-
-    // System JS
+    // System JS Config
     gulp.src(['./systemJS/systemjs.config.js'])
-        .pipe(gulp.dest(webRoot + 'js/'))
+        .pipe(gulp.dest(webRoot + 'js/'));
 
     // SystemJS Needed files
-    gulp.src([paths.nodeModules + '@angular/core/bundles/core.umd.js'])
-        .pipe(gulp.dest(webRoot + 'js/'))
-    gulp.src([paths.nodeModules + '@angular/common/bundles/common.umd.js'])
-        .pipe(gulp.dest(webRoot + 'js/'))
-    gulp.src([paths.nodeModules + '@angular/compiler/bundles/compiler.umd.js'])
-        .pipe(gulp.dest(webRoot + 'js/'))
-    gulp.src([paths.nodeModules + '@angular/platform-browser/bundles/platform-browser.umd.js'])
-        .pipe(gulp.dest(webRoot + 'js/'))
-    gulp.src([paths.nodeModules + '@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js'])
-        .pipe(gulp.dest(webRoot + 'js/'))
-    gulp.src([paths.nodeModules + '@angular/http/bundles/http.umd.js'])
-        .pipe(gulp.dest(webRoot + 'js/'))
-    gulp.src([paths.nodeModules + '@angular/router/bundles/router.umd.js'])
-        .pipe(gulp.dest(webRoot + 'js/'))
-    gulp.src([paths.nodeModules + '@angular/forms/bundles/forms.umd.js'])
-        .pipe(gulp.dest(webRoot + 'js/'))
+
+    // Angular2
+    gulp.src([paths.nodeModules + '@angular/core/bundles/core.umd.js',
+              paths.nodeModules + '@angular/common/bundles/common.umd.js',
+              paths.nodeModules + '@angular/compiler/bundles/compiler.umd.js',
+              paths.nodeModules + '@angular/platform-browser/bundles/platform-browser.umd.js',
+              paths.nodeModules + '@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+              paths.nodeModules + '@angular/http/bundles/http.umd.js',
+              paths.nodeModules + '@angular/router/bundles/router.umd.js',
+              paths.nodeModules + '@angular/forms/bundles/forms.umd.js'])
+        .pipe(gulp.dest(webRoot + 'js/'));
+
+    // Copy across the Reactive Extensions files, in the same structure as they are found (as this needs to be preserved)
     gulp.src([paths.nodeModules + 'rxjs/**/*.js'], { base: "node_modules/rxjs" })
         .pipe(gulp.dest(webRoot + 'js/'))
 });
